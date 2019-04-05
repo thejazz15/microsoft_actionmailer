28c7ad3e569053a426ad5d27d503024279a46ad5
Tejas <tejas.tholpadi7@gmail.com>
2019-04-05 22:43:03 +0530

    add microsoft graph apis to send messages

--- lib/microsoft_actionmailer.rb
+++ lib/microsoft_actionmailer.rb
@@ -1,14 +1,45 @@
 require "microsoft_actionmailer/version"
 require 'microsoft_actionmailer/railtie' if defined?(Rails)
+require 'microsoft_actionmailer/api'
+
+require 'httparty'
+require 'net/http'
+require 'uri'
 
 module MicrosoftActionmailer
-	class DeliveryMethod
-		def initialize mail
 
+	GRAPH_HOST = 'https://graph.microsoft.com'.freeze
+
+	class DeliveryMethod
+		include MicrosoftActionmailer::Api
+
+		attr_reader :access_token
+		attr_reader :delivery_options
+
+		def initialize params
+			@access_token = params[:authorization]
+			@delivery_options = params[:delivery_options] || {}
 		end
 
 		def deliver! mail
+			message = ms_create_message(
+				access_token,
+				mail.subject,
+				mail.body.encoded,
+				mail.to.first
+			)
 
+			before_send = delivery_options[:before_send]
+			if before_send && before_send.respond_to?(:call)
+				before_send.call(mail, message)
+			end
+
+			res = ms_send_message(access_token, message['id'])
+
+			after_send = delivery_options[:after_send]
+			if after_send && after_send.respond_to?(:call)
+				after_send.call(mail, message)
+			end
 		end
 	end
 end
---
+++ lib/microsoft_actionmailer/api.rb
@@ -0,0 +1,56 @@
+module MicrosoftActionmailer
+  module Api
+
+  	# Creates a message and saves in 'Draft' mailFolder
+	  def ms_create_message(token, subject, content, address)
+	    query = {
+        "subject": subject,
+        "importance": "Normal",
+        "body":{
+          "contentType": "HTML",
+          "content": content
+        },
+        "toRecipients": [
+          {
+            "emailAddress": {
+              "address": address
+            }
+          }
+        ]
+      }
+	    create_message_url = '/v1.0/me/messages'
+	    req_method = 'post'
+	    response = make_api_call create_message_url, token, query,req_method
+	    raise response.parsed_response.to_s || "Request returned #{response.code}" unless response.code == 201
+	    response
+	  end
+
+	  # Sends the message created using message id
+	  def ms_send_message(token, message_id)
+	    send_message_url = "/v1.0/me/messages/#{message_id}/send"
+	    req_method = 'post'
+	    query = {}
+	    response = make_api_call send_message_url, token, query,req_method
+	    raise response.parsed_response.to_s || "Request returned #{response.code}" unless response.code == 202
+	    response
+	  end
+
+	  def make_api_call(endpoint, token, params = nil, req_method)
+	    headers = {
+	      'Authorization'=> "Bearer #{token}",
+	      'Content-Type' => 'application/json'
+	    }
+
+	    query = params || {}
+	    if req_method == 'get'
+	      HTTParty.get "#{GRAPH_HOST}#{endpoint}",
+	                 headers: headers,
+	                 query: query
+	    elsif req_method == 'post'
+	      HTTParty.post "#{GRAPH_HOST}#{endpoint}",
+	                 headers: headers,
+	                 body: query.to_json
+	    end
+	  end
+  end
+end
